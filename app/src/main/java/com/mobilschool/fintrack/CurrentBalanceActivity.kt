22aed9403df70cb6fda5b54f9312926e417bf35d
Roman Zelenin <roman_zelenin@hotmail.com>
2018-07-23 00:28:34 +0300

    Init commit

---
+++ app/src/main/java/com/mobilschool/fintrack/CurrentBalanceActivity.kt
@@ -0,0 +1,13 @@
+package com.mobilschool.fintrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_current_balance.*
+
+class CurrentBalanceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_current_balance)
+    }
+}
